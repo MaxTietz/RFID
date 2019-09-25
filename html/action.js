8709f2783404cb23180a1d6588e0514f3b9ad935
d-beck-bkt <45059005+d-beck-bkt@users.noreply.github.com>
2019-09-25 12:51:25 +0200

    changed html file and add js for futur

---
+++ html/action.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+//
+//   document.getElementById("actions").onclick = function() {
+//     var outerdiv = document.createElement("div");
+//     var middiv = document.createElement("div");
+//     var innerdiv = document.createElement("div");
+//     outerdiv.setAttribute("class", "row");
+//     middiv.setAttribute("class", "col-sm-4");
+//     innerdiv.setAttribute("class", "form-group");
+//     var select = document.createElement("select");
+//     var option = document.createElement("option");
+//
+//
+//     select.setAttribute("class", "form-control selection-action");
+//     outerdiv.appendChild(middiv);
+//     middiv.appendChild(innerdiv);
+//     innerdiv.appendChild(select);
+//     select.appendChild(option);
+//     option.setAttribute("value", "Action1");
+//     option.innerText = "Action1";
+//
+//     document.getElementById("actions").appendChild(outerdiv);
+//     return false;
+//   };
+//
+// });
